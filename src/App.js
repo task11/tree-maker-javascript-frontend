6c246c959af8c95dd20f82fa5517f0fac5294a10
task11 <task11@naver.com>
2022-04-19 23:26:51 +0900

    feat: Initializing project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+const App = () => {
+
+  return `
+    <div>
+      <h1>Init Project</h1>
+    </div>
+  `;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from "./App.js";
+
+const render = () => {
+  document.getElementById('root').innerHTML = App();
+};
+
+render();
