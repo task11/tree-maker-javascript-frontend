a7f4a8f02b18bba06ecb41238f4952e33740a1bd
task11 <task11@naver.com>
2022-04-20 17:19:05 +0900

    style: Footer Style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,28 +3,23 @@ const Footer = () => {
 
   return `
   <footer>
-    <div>
-      <div>
+    <div class="footer-logo">
+      <h1>
         <div>
-          <h1>
-            <div>
-              <img src="" alt="DOM tree generator logo">
-              <div>DOM Tree Generator</div>
-            </div>
-          </h1>
+          <img src="https://user-images.githubusercontent.com/89209626/164180885-eb94c33e-5688-4bd8-8189-76c2ef56ab4c.png" alt="DOM tree generator logo">
+          <div>DOM Tree Generator</div>
         </div>
-        <div>
-          <div><strong>How to reach me</strong></div>
-          <div><a href="mailto:6539305@gmail.com">Mail</a></div>
-          <div><a href="https://velog.io/@task11">Blog</a></div>
-          <div><a href="https://github.com/task11">Github</a></div>
-        </div>
-      </div>
-      <div>
-        <span>
-          Developed in Korea 🇰🇷
-        </span>
-      </div>
+      </h1>
+    </div>
+    <div class="footer-contact">
+      <div><strong>How to reach me</strong></div>
+      <div><a href="mailto:6539305@gmail.com">Mail</a></div>
+      <div><a href="https://velog.io/@task11">Blog</a></div>
+      <div><a href="https://github.com/task11">Github</a></div>
+    </div>
+    <div class="footer-author">
+      <div><strong>Author</strong></div>
+      <div><a href="https://task11.github.io/portfolio-javascript-frontend/">Task11 🇰🇷</a></div>
     </div>
   </footer>
   `;
